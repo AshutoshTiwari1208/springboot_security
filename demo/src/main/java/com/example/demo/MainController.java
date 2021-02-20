79fe6b7b54e3f1be3b7ea3b915d93ce06e9a7f1e
Ashutosh <ashutoshtiwari3309@gmail.com>
2021-02-20 22:58:16 +0530

    Admin, user, permitAll Roles - Authentication & Authorization

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -11,4 +11,19 @@ public class MainController {
 		return "<h1>Hey there, what's up?</h1>";
 	}
 	
+	@RequestMapping("/admin")
+	public String getAdminPage() {
+		return "<h2> Yo hoo, Admin's page it is</h2>";
+	}
+	
+	@RequestMapping("/admin/1")
+	public String getAdminPage1() {
+		return "<h2> Exact page - Admin, Admin's page 1 it is</h2>";
+	}
+	
+	@RequestMapping("/user")
+	public String getUserPage() {
+		return "<h2>Hey you are a user wohooo</h2>";
+	}
+
 }
--- demo/src/main/java/com/example/demo/SpringBootSecurity.java
+++ demo/src/main/java/com/example/demo/SpringBootSecurity.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,7 +17,11 @@ public class SpringBootSecurity extends WebSecurityConfigurerAdapter{
 		auth.inMemoryAuthentication()
 			.withUser("ashu")
 			.password("ashu123")
-			.roles("USER");
+			.roles("USER")
+			.and()
+			.withUser("admin")
+			.password("admin123")
+			.roles("ADMIN");
 	}
 	
 	@Bean
@@ -24,8 +29,14 @@ public class SpringBootSecurity extends WebSecurityConfigurerAdapter{
 		return NoOpPasswordEncoder.getInstance();
 	}
 	
-
-	
-	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.antMatchers("/admin/**").hasRole("ADMIN")
+			.antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+			.antMatchers("/**").permitAll()
+			.and().formLogin();
+	}
 	
 }
